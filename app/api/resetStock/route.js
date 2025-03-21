1f8935172139f66e880c1ad1760e29350e7ec152
khaizulaftar <khaizulaftar150704@gmail.com>
2025-03-22 00:55:04 +0700

    update

--- app/api/resetStock/route.js
+++ app/api/resetStock/route.js
@@ -1,11 +1,11 @@
-import { NextResponse } from 'next/server'
-import { dbConnect } from '@/lib/db'
-import moment from 'moment-timezone'
+import { dbConnect } from '@/lib/db';
+import { NextResponse } from 'next/server';
+import moment from 'moment-timezone';
 
 export async function GET() {
     try {
-        const db = await dbConnect()
-        const today = moment().tz("Asia/Jakarta").format("YYYY-MM-DD")
+        const db = await dbConnect();
+        const today = moment().tz("Asia/Jakarta").format("YYYY-MM-DD");
 
         // Query untuk reset stok
         await db.execute(`
@@ -15,11 +15,11 @@ export async function GET() {
             final_stock = 0, 
             out_stock = 0, 
             tanggal = ?
-        WHERE tanggal < ?
-        `, [today, today])
+        WHERE tanggal < ?;
+    `, [today, today]);
 
-        return NextResponse.json({ message: "Stok berhasil direset." })
+        return NextResponse.json({ message: "Stok berhasil direset." });
     } catch (error) {
-        return NextResponse.json({ error: error.message }, { status: 500 })
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
